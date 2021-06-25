bcfb8cd25820a494b0051e1c89f6a3ca184ef20a
fricodelco <rodion_anisimov@mail.ru>
2021-06-25 17:09:09 +0300

    initial comm for this branch

--- AR_HT_api/AR_HT_api/visualization.py
+++ AR_HT_api/AR_HT_api/visualization.py
@@ -13,21 +13,18 @@ from rclpy.qos import ReliabilityPolicy, QoSProfile
 import json
 from std_msgs.msg import Bool, Float32
 from time import sleep, time
-class AR_HT_api(Node):
+
+class GetVisualization(Node):
     def __init__(self):
-        super().__init__('ar_api')
+        super().__init__('get_visualization')
         self.subscription_1 = self.create_subscription(OccupancyGrid,'map',self.map_callback,10)
         self.subscription_2 = self.create_subscription(TFMessage,'tf',self.tf_callback,10)
         self.subscription_3 = self.create_subscription(LaserScan,'scan_1',self.scan_callback_1,1)
         self.subscription_4 = self.create_subscription(LaserScan,'scan_2',self.scan_callback_2, QoSProfile(depth=10, reliability=ReliabilityPolicy.BEST_EFFORT))
         self.subscription_5 = self.create_subscription(OccupancyGrid,'keepout_filter_mask',self.keepout_callback,10)
         self.subscription_6 = self.create_subscription(Path,'plan',self.plan_callback,10)
-        self.subscription_7 = self.create_subscription(JointState,'joint_states',self.js_callback,10)
-        self.subscription_8 = self.create_subscription(Float32,'battery_voltage',self.battery_callback,10)
-        self.create_timer(3.0, self.timer_callback)
         self.tfBuffer = tf2_ros.Buffer()
         self.tf_listener = tf2_ros.TransformListener(self.tfBuffer, self)
-        self.lift_pub = self.create_publisher(Bool, 'lift', 1)
         self.map_ = None
         self.keepout_map = None
         self.tf_time = 0
@@ -39,25 +36,17 @@ class AR_HT_api(Node):
         self.scan_flag_2 = False
         self.plan = []
         self.plan_flag = False
-        self.js_flag = False
-        self.battery_voltage = 0.0
-    def battery_callback(self, data):
-        self.battery_voltage = data.data
-    def js_callback(self, data):
-        self.js_flag = True
+        
     def map_callback(self, data):
         self.map_ = data
+
     def keepout_callback(self, data):
         self.keepout_map = data
-    def timer_callback(self):
-        self.js_flag = False
-        self.battery_voltage = False
-        self.scan_flag_2 = False
-        self.scan_flag_1 = False
-        self.plan_flag = False
+
     def tf_callback(self, tf):
         if tf.transforms[0].child_frame_id == 'base_link':
             self.tf_time = tf.transforms[0].header.stamp
+            
     def scan_callback_1(self, data):
         increment = int(len(data.ranges)/len(self.scan_buffer_1))                
         j = 0
@@ -66,6 +55,7 @@ class AR_HT_api(Node):
             self.scan_angles_1[j] = i*data.angle_increment + data.angle_min
             j+=1
         self.scan_flag_1 = True
+
     def scan_callback_2(self, data):
         increment = int(len(data.ranges)/len(self.scan_buffer_2))                
         j = 0
@@ -74,12 +64,14 @@ class AR_HT_api(Node):
             self.scan_angles_2[j] = i*data.angle_increment + data.angle_min
             j+=1
         self.scan_flag_2 = True
+
     def plan_callback(self, data):
         self.plan = []
         for pose in plan.poses:
             x = pose.pose.position.x 
             y = pose.pose.position.y
             self.plan.append([x,y])
+
     def get_map(self):
         if self.keepout_map is not None:
             map_loc = self.keepout_map 
@@ -97,11 +89,12 @@ class AR_HT_api(Node):
                     blank_image[i][j] = 0
                 else:
                     blank_image[i][j] = 200
-        answr = {'map': blank_image, 
+        answr = {'map': blank_image.tolist(), 
                 'map_origin': (map_loc.info.origin.position.x, map_loc.info.origin.position.y, 0),
                 'map_width': map_loc.info.width,
                 'map_height': map_loc.info.height}
         return answr
+
     def get_scans(self):
         if self.scan_flag_1 == True and self.scan_flag_2 == True:
             answr = {'scan_1_ranges': self.scan_buffer_1, 'scan_1_angles': self.scan_angles_1,
@@ -109,6 +102,7 @@ class AR_HT_api(Node):
             return answr  
         else:
             return False
+
     def get_robot_pose(self):
         try:
             trans = self.tfBuffer.lookup_transform('map', 'base_link', self.tf_time)
@@ -122,47 +116,23 @@ class AR_HT_api(Node):
         except Exception as e: 
             return False
     def get_plan(self):
+
         if self.plan_flag == True:
             answr = {'plan': self.plan}
             return answr
         else:
             return False
-    def get_status(self):
-        if self.battery_voltage == 0:
-            battery = False
-        else:
-            battery = self.battery_voltage
-        answr = {'motors': self.js_flag,
-                'scan_1': self.scan_flag_1,
-                'scan_2': self.scan_flag_2,
-                'lift': True,
-                'battery': battery}
-
-    def lift(self, state, sleep_time=0): #sleep time = 12 for full cycle of up and down
-        msg = Bool()
-        if state == True:
-            msg.data = True
-            self.lift_pub.publish(msg)
-            sleep(sleep_time)
-            return True
-        elif state == False:
-            msg.data = False
-            self.lift_pub.publish(msg)
-            sleep(sleep_time)
-            return True
-        return False
-
   
 def main(args=None):
     rclpy.init(args=args)
-    api = AR_HT_api()
+    api = GetVisualization()
     thread = threading.Thread(target=rclpy.spin, args=(api, ), daemon=True)
     thread.start()
     rate = api.create_rate(2)
     # future = action_client.send_goal(-1.85,-1.12,5.1)
     while rclpy.ok():
         try:
-            api.get_map()
+            print(api.get_map())
             for i in range(0,1):
                 print()
             rate.sleep()
